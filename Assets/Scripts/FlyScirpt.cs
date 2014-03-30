1fff255d3abaeb031e0238e9533174e0095bd981
taesiri <taesiri@live.com>
2014-03-30 23:12:06 +0430

    FlyScript and GameManager

---
+++ Assets/Scripts/FlyScirpt.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class FlyScirpt : MonoBehaviour
+    {
+        public float VerticalSpeed = 1.0f;
+        public float HorizontalSpeed = 10.0f;
+        public float GyroSpeed = 2.0f;
+        public Transform PasheCam;
+        public GUISkin MenuSkin;
+        public GUILocationHelper Location = new GUILocationHelper();
+        public GameManager Gmanger;
+
+
+        private void Start()
+        {
+            if (!PasheCam)
+            {
+                Debug.LogError("Pashe Cam not found");
+            }
+            if (!Gmanger)
+            {
+                Debug.LogError("Game manager not found");
+            }
+
+            Location.PointLocation = GUILocationHelper.Point.Center;
+            Location.UpdateLocation();
+
+            Input.gyro.enabled = true;
+        }
+
+
+        private void Update()
+        {
+            transform.position += Vector3.up*VerticalSpeed*Time.deltaTime;
+            PasheCam.position += Vector3.up*VerticalSpeed*Time.deltaTime;
+
+            HandleInputs();
+        }
+
+        private void HandleInputs()
+        {
+            var xO = Input.GetAxis("Horizontal");
+
+            transform.position += Vector3.right*xO*HorizontalSpeed*Time.deltaTime;
+
+
+#if UNITY_IPHONE || UNITY_ANDROID
+            //Boundary checks required 
+            transform.position += Vector3.right*Time.deltaTime*GyroSpeed*GyroSign(Input.gyro.attitude.x);
+#endif
+        }
+
+
+        public void OnGUI()
+        {
+            Vector2 ratio = Location.GuiOffset;
+            Matrix4x4 guiMatrix = Matrix4x4.identity;
+            guiMatrix.SetTRS(new Vector3(1, 1, 1), Quaternion.identity, new Vector3(ratio.x, ratio.y, 1));
+            GUI.matrix = guiMatrix;
+
+         
+
+            GUI.matrix = Matrix4x4.identity;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.gameObject.name);
+        }
+
+
+        private int GyroSign(float value)
+        {
+            if (Mathf.Abs(value) < 0.01f) return 0;
+            if (value > 0) return 1;
+            if (value < 0) return -1;
+
+            return 0;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        private Random _rndGenerator = new Random(DateTime.Now.Millisecond);
+        private float _score = 10f;
+        public GameObject[] Piffs;
+        public Transform PasheTransform;
+        public int DeployInterval = 1;
+        private float _lastTick;
+
+        public void Start()
+        {
+            _lastTick = Time.time;
+        }
+
+        public void Update()
+        {
+            if (Time.time - _lastTick > DeployInterval)
+            {
+                CreatePiff();
+                _lastTick = Time.time;
+            }
+        }
+
+        private void CreatePiff()
+        {
+            var index = _rndGenerator.Next(0, Piffs.Length);
+            Instantiate(Piffs[index], PasheTransform.position + Vector3.up*5, Piffs[index].transform.rotation);
+        }
+    }
+}
